44e7ed3a74481b6d0c828713da6f0501b7a08f97
Ph0m1 <2732706745@qq.com>
2025-04-22 20:15:54 +0800

    add example to mux

---
+++ examples/mux/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ph0m1/p_gateway/config/viper"
+	"github.com/ph0m1/p_gateway/logging/gologging"
+	"github.com/ph0m1/p_gateway/proxy"
+	"github.com/ph0m1/p_gateway/router/mux"
+)
+
+func main() {
+	port := flag.Int("p", 0, "Port of the service")
+	logLevel := flag.String("l", "ERROR", "Logging level")
+	debug := flag.Bool("d", false, "Enable the debug")
+	configFile := flag.String("c", "configuration.json", "Path to configuration filename")
+	flag.Parse()
+
+	parser := viper.New()
+	serviceConfig, err := parser.Parse(*configFile)
+	if err != nil {
+		log.Fatal("ERROR:", err.Error())
+	}
+
+	serviceConfig.Debug = serviceConfig.Debug || *debug
+	if *port != 0 {
+		serviceConfig.Port = *port
+	}
+	logger := gologging.NewLogger(*logLevel, os.Stdout, "[O.o]")
+
+	routerFactory := mux.DefaultFactory(proxy.DefaultFactory(logger), logger)
+	routerFactory.New().Run(serviceConfig)
+}
